ba84a51e45791300147e3efe45a52d0106251a11
Ruben Rangel <rubenmrangel@gmail.com>
2024-11-24 12:00:05 -0800

    start preparing NPM packaging

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,3 @@
-import * as fs from "node:fs";
-import {parseHighlights} from "./StreamOutputPraser";
-import {MarkdownFormatter} from "./Formatters";
-
-
-const csvStream = fs.createReadStream('../building-evolutionary-architectures-oreilly-annotations.csv');
-const outputFile = fs.createWriteStream('../output.md', {flags: 'a'});
-
-const formatter = new MarkdownFormatter();
-
-(async () => {
-  for await (const highlight of parseHighlights(csvStream)) {
-    const formatted = formatter.transform(highlight);
-    outputFile.write(`${formatted}\n\n`);
-  }
-
-  outputFile.end();
-})();
+export * from './formatter';
+export * from './parser';
+export * from './OreillyHighlight';
